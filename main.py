73690d8ad0799870d1af173072095275ad43cf05
miguel <mdevia@cesde.net>
2024-02-06 18:54:30 -0500

    se agrega el valor de la hora extra

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ horas_trabajadas = float(input("cuantas horas trabajo"))
 salario_base =int(input("valor hora"))
 horas_extra = float(input("Ingrese las horas extra trabajadas: "))
 valor_hora_extra = float(input("Ingrese el valor por hora extra: "))
+
+salario_base_total = horas_trabajadas * salario_base
+salario_extra = horas_extra * valor_hora_extra
+salario_total = salario_base_total + salario_extra
