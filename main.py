1af83955bc8f02e9d09b1335ce3dc1f73767e8ec
miguel <mdevia@cesde.net>
2024-02-06 18:13:06 -0500

    inicio, se crean variables e inputs de datos basicos

---
+++ main.py
@@ -0,0 +1,5 @@
+nombre = input("ingrese su nombre")
+edad = int(input("ingrese su edad"))
+ciudad = input("cual es su ciudad")
+horas_trabajadas = float(input("cuantas horas trabajo"))
+salario_base =int(input("valor hora"))
