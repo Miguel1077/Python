f0a4b8fa83dda71b1508ab9044b49f73ba4c4f4e
miguel <mdevia@cesde.net>
2024-02-06 19:01:03 -0500

    se finaliza agregando los respectivos print's

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-nombre = input("ingrese su nombre")
-edad = int(input("ingrese su edad"))
-ciudad = input("cual es su ciudad")
-horas_trabajadas = float(input("cuantas horas trabajo"))
-salario_base =int(input("valor hora"))
+nombre = input("ingrese su nombre: ")
+edad = int(input("ingrese su edad: "))
+ciudad = input("cual es su ciudad: ")
+horas_trabajadas = float(input("cuantas horas trabajo: "))
+salario_base =int(input("valor hora: "))
 horas_extra = float(input("Ingrese las horas extra trabajadas: "))
 valor_hora_extra = float(input("Ingrese el valor por hora extra: "))
 
 salario_base_total = horas_trabajadas * salario_base
 salario_extra = horas_extra * valor_hora_extra
 salario_total = salario_base_total + salario_extra
+
+
+print("\nColilla de Pago")
+print("Nombre: {}".format(nombre))
+print("Edad: {}".format(edad))
+print("Ciudad: {}".format(ciudad))
+print("Horas Trabajadas: {}".format(horas_trabajadas))
+print("Salario Base por Hora: ${}".format(salario_base))
+print("Salario Base Total: ${}".format(salario_base_total))
+print("Horas Extra: {}".format(horas_extra))
+print("Valor Hora Extra: ${}".format(valor_hora_extra))
+print("Salario por Horas Extra: ${}".format(salario_extra))
+print("Salario Total: ${}".format(salario_total))
